4528d0d4458dead8f8a23169ca13473c86c3bc04
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-10-18 17:37:29 +0800

    Add files via upload

---
+++ Lab3__Part3_SourceCode/BankOne/LoanAccount.java
@@ -0,0 +1,8 @@
+
+public class LoanAccount extends Account{
+    
+    public LoanAccount(String _accountNumber , double _balance){
+        super(_accountNumber,_balance);
+    }
+
+}
---
+++ Lab3__Part3_SourceCode/BankOne/SavingAccount.java
@@ -0,0 +1,8 @@
+
+public class SavingAccount extends Account{
+    
+    public SavingAccount(String _accountNumber , double _balance){
+        super(_accountNumber,_balance);
+    }   
+
+}
