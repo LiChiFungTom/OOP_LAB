b50fc4ff611fd4c9c2442bebcb8c1e15d322d71a
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-10-18 17:36:43 +0800

    Delete Lab3__Part3_SourceCode directory

--- Lab3__Part3_SourceCode/BankOne/LoanAccount.java
+++
@@ -1,9 +0,0 @@
-package BankOne;
-
-public class LoanAccount extends Account{
-    
-    public LoanAccount(String _accountNumber , double _balance){
-        super(_accountNumber,_balance);
-    }
-
-}
--- Lab3__Part3_SourceCode/BankOne/SavingAccount.java
+++
@@ -1,9 +0,0 @@
-package BankOne;
-
-public class SavingAccount extends Account{
-    
-    public SavingAccount(String _accountNumber , double _balance){
-        super(_accountNumber,_balance);
-    }   
-
-}
